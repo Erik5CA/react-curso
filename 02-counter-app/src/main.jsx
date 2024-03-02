682453fec4a6cbd29824df5a57a3c99db5d0c10e
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-02 16:28:52 -0600

    primer componente

--- 02-counter-app/src/main.jsx
+++ 02-counter-app/src/main.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-    return(
-        <h1>Hola Mundo!!</h1>
-    )
-}
+import {App} from './HelloWordApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
