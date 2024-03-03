85b76a7786298d85ffb8cc18827610c605f58446
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-02 18:57:24 -0600

    tipado de props

--- 02-counter-app/src/FirstApp.jsx
+++ 02-counter-app/src/FirstApp.jsx
@@ -1,24 +1,21 @@
-/* eslint-disable react/prop-types */
-const newMessage = "Fernando";
-const objeto = {
-  nombre: "Erik",
-  apellido: "Castillo",
-};
+import { PropTypes } from "prop-types";
 
-const getResult = () => {
-  return 4 + 4;
-};
-
-export function FirstApp({ title = "Hola soy Erik", subtitle }) {
+export function FirstApp({ title, subtitle }) {
   // console.log(props);
+
+  // if (title) {
+  //   throw new Error("El titulo no existe");
+  // }
+
   return (
     <>
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
-      <h1>First App {newMessage}</h1>
-      <code>{JSON.stringify(objeto)}</code>
-      <p>Soy un subtitulo</p>
-      <p>Una Función {getResult()}</p>
     </>
   );
 }
+
+FirstApp.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+};
--- 02-counter-app/src/main.jsx
+++ 02-counter-app/src/main.jsx
@@ -7,6 +7,6 @@ import "./styles.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <FirstApp title="Erik" subtitle="123" />
+    <FirstApp title={123} />
   </React.StrictMode>
 );
