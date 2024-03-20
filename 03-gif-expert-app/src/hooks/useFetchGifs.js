94e4e50ce527f85d68ac1e0c9334dd66d23f5901
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-20 13:03:44 -0600

    agrapación de hooks react

--- 03-gif-expert-app/src/hooks/useFetchGifs.js
+++ 03-gif-expert-app/src/hooks/useFetchGifs.js
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const useFetchGifs = (category) => {
