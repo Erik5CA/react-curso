a26e4567c3f22385b46462682fd590968969eabd
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-20 13:03:04 -0600

    Mensaje de loading

--- 03-gif-expert-app/src/components/GifGrid.jsx
+++ 03-gif-expert-app/src/components/GifGrid.jsx
@@ -8,6 +8,7 @@ export function GifGrid({ category }) {
   return (
     <>
       <h3>{category}</h3>
+      {isLoading && <h2>Cargando..</h2>}
       <div className="card-grid">
         {gifs?.map((image) => (
           <GifItem key={image.id} {...image} />
